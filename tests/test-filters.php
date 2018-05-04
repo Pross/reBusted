16c4d6c1f2463355b091b45d48ed8b2ee870ee2e
Simon Prosser <pross@pross.org.uk>
2018-05-04 18:12:50 +0100

    Fix broken tests.

--- tests/test-filters.php
+++ tests/test-filters.php
@@ -13,10 +13,10 @@ class BustedFilters extends WP_UnitTestCase {
 	}
 
 	public function test_stylesheet_uri() {
-		$this->assertEquals( Re_Busted::get_priority(), has_filter( 'stylesheet_uri', array( 'Re_Busted', 'stylesheet_uri' ) ) );
+		$this->assertEquals( PHP_INT_MAX - 1, has_filter( 'stylesheet_uri', array( 'Re_Busted', 'stylesheet_uri' ) ) );
 	}
 
 	public function test_locale_stylesheet_uri() {
-		$this->assertEquals( Re_Busted::get_priority(), has_filter( 'locale_stylesheet_uri', array( 'Re_Busted', 'stylesheet_uri' ) ) );
+		$this->assertEquals( PHP_INT_MAX - 1, has_filter( 'locale_stylesheet_uri', array( 'Re_Busted', 'stylesheet_uri' ) ) );
 	}
 }
